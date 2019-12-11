faebf8180f72b8d22d3e54c8b3dd492733ab9e5b
Boringboys <35089626+Boringboys@users.noreply.github.com>
2019-12-11 23:21:25 +0800

    update

--- assets/js/markdown-edit.js
+++ assets/js/markdown-edit.js
@@ -1,6 +1,6 @@
 $(function(){
                 var markdown_height = window.innerHeight-document.getElementById('header').offsetHeight;
-                document.getElementById('markdown').style.height=markdown_height+"px";
+                document.getElementById('markdown').style.height=markdown_height-50+"px";
 
                 var con=new showdown.Converter();
                 var text=$('#markdown-edit').val();
@@ -9,7 +9,7 @@ $(function(){
 
             window.onresize=function(){
                 var markdown_height = window.innerHeight-document.getElementById('header').offsetHeight;
-                document.getElementById('markdown').style.height=markdown_height+"px";
+                document.getElementById('markdown').style.height=markdown_height-50+"px";
             }
 
             function textchange(){
