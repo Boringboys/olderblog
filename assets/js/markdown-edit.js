853d7b8769601b94bdfb35a95b95e3b76993876c
Boringboys <35089626+Boringboys@users.noreply.github.com>
2019-12-12 20:44:05 +0800

    导入导出基本完成

--- assets/js/markdown-edit.js
+++ assets/js/markdown-edit.js
@@ -26,10 +26,29 @@ $(function(){
 			
 			function savebtnonclicked(){
 				console.log(456);
+				var savetext = $('#markdown-edit').val();
+				if(savetext){
+					console.log("请输入文件名");
+					var savefilename = prompt("请输入要保存的文件名","newFile.md");
+					console.log(savefilename);
+					if(savefilename){
+						var blob = new Blob([savetext],{type:"text/plain;charset=utf-8"});				
+					saveAs(blob,savefilename);
+					}else{
+						//无文件名
+					}
+				}else{
+					//无内容
+				}
+				
+				
 			}
 			
 			function cleanbtnonclicked(){
 				console.log(789);
+				$('#markdown-edit').val('');
+				$('#inner_main').empty();
+				//document.getElementById('inner_main').innerHTML="";
 			}
 			
 			function fileimport(){
