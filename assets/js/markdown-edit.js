dfb206cbbbae2f1860ed831305261a82aa9b211e
Boringboys <35089626+Boringboys@users.noreply.github.com>
2019-12-12 12:57:10 +0800

    update

--- assets/js/markdown-edit.js
+++ assets/js/markdown-edit.js
@@ -17,3 +17,31 @@ $(function(){
                 var text=$('#markdown-edit').val();
                 document.getElementById('inner_main').innerHTML=con.makeHtml(text);
             }
+			
+			function openbtnonclicked(){
+				console.log("点击打开按钮，选择文件");
+				document.getElementById('fileselect').click();
+				
+			}
+			
+			function savebtnonclicked(){
+				console.log(456);
+			}
+			
+			function cleanbtnonclicked(){
+				console.log(789);
+			}
+			
+			function fileimport(){
+				console.log("文件已选择，导入文件");
+				var selectfile = document.getElementById('fileselect').files[0];
+				var reader = new FileReader();
+				reader.readAsText(selectfile);
+				reader.onload = function(){
+					console.log(this.result);
+					document.getElementById('markdown-edit').value=this.result;
+					var con=new showdown.Converter();
+					var text=$('#markdown-edit').val();
+					document.getElementById('inner_main').innerHTML=con.makeHtml(text);
+				}				
+			}
